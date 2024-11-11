57a6926fe52228b2aef76b4726405c72f6e596f7
yaswanthrajeev <yaswanthrajeev@gmail.com>
2024-11-11 22:21:22 +0530

    footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import AboutPage from './pages/AboutPage';
 import Community from './pages/Community';
 import RedeemPage from './pages/RedeemPage';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           <Route path="/community" element={<Community />} />
           <Route path="/redeem" element={<RedeemPage />} />
         </Routes>
+        <Footer/>
       </div>
     </Router>
   );
