5473583ab44e866cd508ee57e8f010408cc5a24e
yaswanthrajeev <yaswanthrajeev@gmail.com>
2024-11-11 21:32:53 +0530

    implemented routing for pages

---
+++ frontend/my-app/src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+// src/pages/HomePage.js
+
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+function HomePage() {
+  return (
+    <div>
+        
+      <h1>Welcome to the Event Management Platform</h1>
+      <p>This is the Home Page</p>
+    </div>
+  );
+}
+
+export default HomePage;
